6da3f626c51e4e6b225c7617fd6138a5faa9bb95
coderaiser <coderaiser@cloudcmd.io>
2018-08-13 14:29:53 +0300

    chore(docs) var -> const

--- release/docs.js
+++ release/docs.js
@@ -6,29 +6,28 @@ const place = require('place');
 const rendy = require('rendy');
 const shortdate = require('shortdate');
 const files = require('files-io');
-const Info = require(DIR + 'package');
+const {version} = require(DIR + 'package');
 
 module.exports = (callback) => {
-    let name        = 'Історія версій';
-    let  history    = name + '\n---------------\n';
-    let link        = '//github.com/coderaiser/cloudcmd/releases/tag/';
-    let template    = '- *{{ date }}*, '    +
-                  '**[v{{ version }}]'   +
+    const name = 'Історія версій';
+    const history = name + '\n---------------\n';
+    const link = '//github.com/coderaiser/cloudcmd/releases/tag/';
+    const template = '- *{{ date }}*, '     +
+                  '**[v{{ version }}]'      +
                   '(' + link + 'v{{ version }})**\n';
-    let version     = Info.version;
     
     cl((error, versionNew) => {
         if (error)
             return callback(error);
         
         replaceVersion('index.md', version, versionNew, () => {
-            var historyNew = history + rendy(template, {
+            const historyNew = history + rendy(template, {
                 date        : shortdate(),
                 version     : versionNew
             });
             
             replaceVersion('index.md', history, historyNew, () => {
-                var to = rendy('v{{ version }}.md', {
+                const to = rendy('v{{ version }}.md', {
                     version: versionNew
                 });
                 
